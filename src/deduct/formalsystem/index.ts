be32c71845ce92ceb7a2e8cc46b4b10b8a61974e
VeryrrDefine <1308061599@qq.com>
2026-04-01 22:57:40 +0800

    use abbrs

--- src/deduct/formalsystem/index.ts
+++ src/deduct/formalsystem/index.ts
@@ -1,12 +1,12 @@
 import {
-	AnyPropositionAST,
-	ImplicationPropositionAST,
-	LetterPropositionAST,
-	type Proposition,
+	AnyPropositionAST as AnyProp,
+	ImplicationPropositionAST as Impl,
+	LetterPropositionAST as LetterProp,
+	type Proposition as Prop,
 } from '../parser/ast';
 class MatchError extends Error {}
 type MatchTable = {
-	[key: string]: Proposition;
+	[key: string]: Prop;
 };
 export class FormalSystem {
 	static debugLog(...args: any[]) {
@@ -20,10 +20,10 @@ export class FormalSystem {
 	 * @param currentAST
 	 * @param goalAST
 	 */
-	static match(currentAST: Proposition, goalAST: Proposition, anyPropositionMap: MatchTable) {
+	static match(currentAST: Prop, goalAST: Prop, anyPropositionMap: MatchTable) {
 		FormalSystem.debugLog(`Matching ${currentAST} to ${goalAST}`);
 
-		if (goalAST instanceof AnyPropositionAST) {
+		if (goalAST instanceof AnyProp) {
 			if (this.freeEquals(currentAST, anyPropositionMap[goalAST.name])) {
 				FormalSystem.debugLog(
 					`Matched ${goalAST.name} to goalAST(left) ${currentAST.toString()}.`,
@@ -37,33 +37,24 @@ export class FormalSystem {
 			return;
 		}
 
-		if (
-			currentAST instanceof ImplicationPropositionAST &&
-			goalAST instanceof ImplicationPropositionAST
-		) {
+		if (currentAST instanceof Impl && goalAST instanceof Impl) {
 			this.match(currentAST.left, goalAST.left, anyPropositionMap);
 			this.match(currentAST.right, goalAST.right, anyPropositionMap);
 			FormalSystem.debugLog(`Matched implication: ${currentAST} and ${goalAST}`);
 			return;
 		}
 		// 如果一个是蕴含另一个不是，则抛出类型不匹配
-		if (
-			currentAST instanceof ImplicationPropositionAST ||
-			goalAST instanceof ImplicationPropositionAST
-		) {
+		if (currentAST instanceof Impl || goalAST instanceof Impl) {
 			throw new MatchError('Proposition type mismatch: implication vs non-implication');
 		}
 
-		if (currentAST instanceof LetterPropositionAST && goalAST instanceof LetterPropositionAST) {
+		if (currentAST instanceof LetterProp && goalAST instanceof LetterProp) {
 			if (currentAST.name !== goalAST.name) {
 				throw new MatchError('Letter proopsition mismatch.');
 			}
 			return;
 		}
-		if (
-			currentAST instanceof ImplicationPropositionAST ||
-			goalAST instanceof ImplicationPropositionAST
-		) {
+		if (currentAST instanceof Impl || goalAST instanceof Impl) {
 			throw new MatchError('Proposition type mismatch: letter vs non-letter');
 		}
 	}
@@ -72,7 +63,7 @@ export class FormalSystem {
 	 *
 	 * @returns
 	 */
-	static freeEquals(leftAST: Proposition | undefined, rightAST: Proposition | undefined) {
+	static freeEquals(leftAST: Prop | undefined, rightAST: Prop | undefined) {
 		if (leftAST === undefined) return true;
 		if (rightAST === undefined) return true;
 		return leftAST?.toString?.() === rightAST?.toString?.();
@@ -84,37 +75,30 @@ export class FormalSystem {
 	 * @param $0 $0
 	 * @returns $1
 	 */
-	static ruleModusPonens($0imp$1: Proposition, $0: Proposition) {
+	static ruleModusPonens($0imp$1: Prop, $0: Prop) {
 		const matchTable: MatchTable = {};
 		FormalSystem.debugLog(`Matching modus ponens: ${$0imp$1}, ${$0} ⊢ ???`);
-		this.match(
-			$0imp$1,
-			new ImplicationPropositionAST(new AnyPropositionAST('$0'), new AnyPropositionAST('$1')),
-			matchTable,
-		);
+		this.match($0imp$1, new Impl(new AnyProp('$0'), new AnyProp('$1')), matchTable);
 
-		this.match($0, new AnyPropositionAST('$0'), matchTable);
+		this.match($0, new AnyProp('$0'), matchTable);
 
 		return matchTable['$1'];
 	}
 
-	static ruleA1($0: Proposition, $1: Proposition) {
-		return new ImplicationPropositionAST($0, new ImplicationPropositionAST($1, $0));
+	static ruleA1($0: Prop, $1: Prop) {
+		return new Impl($0, new Impl($1, $0));
 	}
-	static ruleA2($0: Proposition, $1: Proposition, $2: Proposition) {
-		return new ImplicationPropositionAST(
-			new ImplicationPropositionAST($0, new ImplicationPropositionAST($1, $2)),
-			new ImplicationPropositionAST(
-				new ImplicationPropositionAST($0, $1),
-				new ImplicationPropositionAST($0, $2),
-			),
+	static ruleA2($0: Prop, $1: Prop, $2: Prop) {
+		return new Impl(
+			new Impl($0, new Impl($1, $2)),
+			new Impl(new Impl($0, $1), new Impl($0, $2)),
 		);
 	}
 
-	static ruleDotI($0: Proposition) {
+	static ruleDotI($0: Prop) {
 		const theorem1 = this.ruleA1($0, $0);
-		const theorem2 = this.ruleA1($0, new ImplicationPropositionAST($0, $0));
-		const theorem3 = this.ruleA2($0, new ImplicationPropositionAST($0, $0), $0);
+		const theorem2 = this.ruleA1($0, new Impl($0, $0));
+		const theorem3 = this.ruleA2($0, new Impl($0, $0), $0);
 		const theorem4 = this.ruleModusPonens(theorem3, theorem2);
 		const theorem5 = this.ruleModusPonens(theorem4, theorem1);
 		return theorem5;
