bf801a151401275aa3b4a5f0d75a57b4eb3a1722
VeryrrDefine <1308061599@qq.com>
2026-04-03 21:22:50 +0800

    fx bg

--- src/deduct/formalsystem/index.ts
+++ src/deduct/formalsystem/index.ts
@@ -23,7 +23,7 @@ export class FormalSystem {
 	} = {
 		mp: FormalSystemRule.fromString('$0>$1, $0|-$1', 'mp'),
 		a1: FormalSystemRule.fromString('|-$0>($1>$0)', 'a1'),
-		a2: FormalSystemRule.fromString('|-($0>($1>$2))>(($0>$1)>($0>$2))', ' a2'),
+		a2: FormalSystemRule.fromString('|-($0>($1>$2))>(($0>$1)>($0>$2))', 'a2'),
 		a3: FormalSystemRule.fromString('|-(~$0>~$1)>($1>$0)', 'a3'),
 		'd<>1': FormalSystemRule.fromString('|-($0<>$1)>~(($0>$1)>~($1>$0))', 'd<>1'),
 		'd<>2': FormalSystemRule.fromString('|-~(($0>$1)>~($1>$0))>($0<>$1)', 'd<>2'),
@@ -174,7 +174,7 @@ export class FormalSystem {
 				operation:
 					step.rule_id +
 					(step.chosen_condition.length !== 0
-						? ` ` + step.chosen_condition.join(', ')
+						? ' ' + step.chosen_condition.join(', ')
 						: ''),
 			});
 		}
