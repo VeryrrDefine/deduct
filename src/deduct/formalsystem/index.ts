92f211050cb4dee83452997d7d30a58d7c1fbe53
VeryrrDefine <1308061599@qq.com>
2026-04-04 20:21:35 +0800

    add 注释

--- src/deduct/formalsystem/index.ts
+++ src/deduct/formalsystem/index.ts
@@ -532,6 +532,7 @@ export class FormalSystem {
 			this.removePropositions(1 / 0);
 			this.hypothesis = [];
 
+			// 首先添加前几个假设，（没有最后一个）
 			d.condition.forEach((c, id) => {
 				if (id !== d.condition.length - 1) {
 					this.addHypothesis(c);
@@ -539,9 +540,15 @@ export class FormalSystem {
 					s = c;
 				}
 			});
-
+			//最后一个假设记为s
+			/**
+			 * 存储原步骤与新步骤间的关系
+			 */
 			let stepPayload: number[] = [];
 
+			/**
+			 * 存储原假设和新构造的s->假设的关系
+			 */
 			let hypothesis_conditioned: number[] = [];
 			for (let hyp2 = 0; hyp2 < d.condition.length - 1; hyp2++) {
 				hypothesis_conditioned.push(
@@ -555,42 +562,42 @@ export class FormalSystem {
 				);
 			}
 			console.log(hypothesis_conditioned);
-			// 首先考虑怎么构建假设s的特殊物，($2>$0)>($2>$0)是可以的
+			// 构造s->s
 			hypothesis_conditioned.push(this.deduct('.i', { '0': s }, [])[1]);
 
 			for (let m = 0; m < d.steps.length; m++) {
-				/**
-				 * X必须符合如下条件之一：
-				 * 1.X是一个已有的假设
-				 * 2.X是一个逻辑公理
-				 * 3.在这一行前存在两行分别形如a→X、a (mp)
-				 */
-				// 对X(m)分类讨论：
-				// 1.X(m)是一个不是a的假设，或者是一个逻辑公理 （这里step结果不会包含假设，所以一定是一个）
 				const step = d.steps[m];
 
+				// 如果这个步骤所需的条件不包含假设
 				if (!step.chosen_condition.includes(this.hypothesis.length - 1 + 1)) {
+					// 首先构造原先的命题A
 					let thisstep = this.deduct(
 						step.rule_id,
 						step.match_map,
 						step.chosen_condition,
 					)[1];
-					// 构造(s->A)
+					// 然后构造(s->A)，记录A和s->A的映射
 					stepPayload.push(
 						this.deduct('<a1', { '1': s }, [this.relatively(thisstep)])[1],
 					);
 				} else if (step.proposition.toString() == s.toString()) {
+					// 如果这个命题就是假设本身
+					// 直接记录s和s->s的映射
 					stepPayload.push(hypothesis_conditioned[hypothesis_conditioned.length - 1]);
 				} else {
-					// 3.存在n<m，使得s→X(n),s→(X(n)→X(m))已被证明
-					// 实际上是存在n<m，使得s->A,s->B,s->C,....这些神秘东西可以推出s->Fin
-					// 查找对应的stepPayload，直接/bx/bx/bx起来
+					// 如果这个步骤依赖了s
+
+					// 记当前步骤为T，
+					// 原先 A, B, C, s, ... 可以推出 T
+					// 那么根据条件演绎元定理， 有s->A,s->B,s->C,s->s....推出s->T。
+					// 查找每一个A,B,C 查找对应的stepPayload
 
 					const t = this;
 					let conditions = step.chosen_condition;
-					console.log(conditions);
 					conditions = conditions.map((x) =>
 						t.relatively(
+							// 筛选，如果x>=0说明是假设，就去假设列表里找，如果<0那么去stepPayload找
+
 							x >= 0 ? hypothesis_conditioned[x] : stepPayload[t.absolutely(x)],
 						),
 					); // 这里的conditions的有时候会选到一个s->Undefined的东西，需要检查
@@ -599,6 +606,7 @@ export class FormalSystem {
 					for (let bx = 0; bx < conditions.length; bx++) {
 						let fn = conditions[bx];
 						if (fn >= 0) {
+							// 表明stepPayload获取到了undefined，产生了NaN
 							if (isNaN(fn)) {
 								throw new Error('Check');
 							} else {
