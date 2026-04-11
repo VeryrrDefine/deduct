3d1a1998138b2655dc6dbcf65242cc62a45af02e
VeryrrDefine <1308061599@qq.com>
2026-04-11 10:33:50 +0800

    2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
+import { FormalSystem } from './deduct/formalsystem';
 import parser from './deduct/parser';
+import { toProposition, toRule } from './deduct/parser/compiler';
 
 const deduct = {
 	parser,
+	FormalSystem,
+	toProposition,
+	toRule,
 };
 export default deduct;
